8ffba4babe14c19df8c01f363fa23051977d37b7
binesh1 <bineshkumar014@gmail.com>
2022-01-24 11:36:27 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+    required_providers {
+        google = {
+            source = "hashicorp/google"
+            version = "4.7.0"
+
+         }
+    }
+
+}
+
+provider "google" {
+    #configuration option
+    project = "gcp-terraform-339116"
+    region = "us-central1"
+    zone =  "us-central1-a"
+    credentials = "keys.json"
+}
+resource google_storage_bucket GCS1 {
+  name = "gcp-terraform-sa"
+  location = "US"
+}
